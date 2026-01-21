b3b66ee9181b3c56c11caaab42d1d3811c9e510c
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2026-01-20 19:12:30 -0800

    stuff

--- Assignment1/Assn1_Task1.py
+++ Assignment1/Assn1_Task1.py
@@ -1,12 +1,28 @@
 from Crypto.Cipher import AES
-from PIL import Image
+
+def read_bmp(path):
+    with open(path, "rb") as f:
+        data = f.read()
+
+    # BMP signature check (optional but helpful)
+    if data[:2] != b"BM":
+        raise ValueError("Not a BMP file")
+
+    # Pixel array offset is stored at bytes 10–13 (little-endian)
+    pixel_offset = int.from_bytes(data[10:14], "little")
+
+    header = data[:pixel_offset]
+    pixels = data[pixel_offset:]
+    return header, pixels
 
 def main():
-    img1=Image.open("Assignment1/images/cp-logo.bmp")
-    img2=Image.open("Assignment1/images/mustang.bmp")
-
-    data
-
+    header,pixels=read_bmp("Assignment1/images/cp-logo.bmp")
+    # img2=Image.open("Assignment1/images/mustang.bmp")
+    if pixels % 16 != 0:
+        padding_length = 16 - (len(pixels) % 16)
+        
+    elif pixels % 16 == 0:
+        padding_length = 0
 
 
 
