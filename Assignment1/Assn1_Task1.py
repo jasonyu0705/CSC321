d45428faaaa9c3be2639f1fd39d272f66774c8d0
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2026-01-20 18:31:46 -0800

    starting task 1

---
+++ Assignment1/Assn1_Task1.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+from PIL import Image
+
+def main():
+    img1=Image.open("Assignment1/images/cp-logo.bmp")
+    img2=Image.open("Assignment1/images/mustang.bmp")
+
+    data
+
+
+
+
+if __name__ == "__main__":
+    main()
